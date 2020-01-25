bc8f57efc5d3282e2fa96ffd7b756a2c9ad672c9
davidmagnussson <davidmagnusson97@gmail.com>
2020-01-25 13:43:30 +0100

    Updates

---
+++ precache-manifest.60d1b26d40493ca464e5881f7886b003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543ef8ce911f9fa126eb230f725e107b",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "a3782dbba7cac7f1bd46",
+    "url": "/project1/static/css/main.1d0f12eb.chunk.css"
+  },
+  {
+    "revision": "b67ad85de2fd5b6f9d20",
+    "url": "/project1/static/js/2.ec4ff571.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/project1/static/js/2.ec4ff571.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3782dbba7cac7f1bd46",
+    "url": "/project1/static/js/main.13b66730.chunk.js"
+  },
+  {
+    "revision": "96c98147192d82a30475",
+    "url": "/project1/static/js/runtime-main.024ca956.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project1/static/media/logo.5d5d9eef.svg"
+  }
+]);
